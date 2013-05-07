c9009cab95d065383799f17cab805d5959762865
Lloyd <lloydmeta@gmail.com>
2013-05-07 13:49:51 +0900

    Add spec helper

---
+++ spec/spec_helper.rb
@@ -0,0 +1,11 @@
+require 'bundler/setup'
+require 'rspec'
+require 'redis_cleaner'
+
+Dir[File.expand_path('../support/**/*', __FILE__)].each { |f| require f }
+
+RSpec.configure do |config|
+
+  #nothing yet
+
+end
